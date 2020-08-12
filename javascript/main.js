3f7c7dbbd0775a01b6091d7016f25502b041d596
Torri Porter <torriporter@Torris-MacBook-Pro.local>
2020-08-12 14:24:09 -0700

    basic page change animations completed

---
+++ javascript/main.js
@@ -0,0 +1,22 @@
+$(document).ready( function() {
+
+  $('.card-body').hide()
+
+  $('.card').mouseenter( function(e) {
+    e.preventDefault();
+    var target = e.target;
+    var info = $(target).children('.card-body');
+    $(info[0]).fadeIn("fast");
+  });
+  $('.card').mouseleave( function(e) {
+    e.preventDefault();
+    var target = e.target;
+    var info = $(target).children('.card-body');
+    $(info[0]).fadeOut("fast");
+  });
+  $(".card").click(function() {
+  window.location = $(this).find("a").attr("href");
+  return false;
+});
+
+});
