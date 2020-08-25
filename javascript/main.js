4bf6189b82500266ca577a94d466ccb346c6b6b8
Torri Porter <torriporter@Torris-MacBook-Pro.local>
2020-08-24 17:45:44 -0700

    started progress on gardener page

--- javascript/main.js
+++ javascript/main.js
@@ -8,15 +8,17 @@ $(document).ready( function() {
     var info = $(target).children('.card-body');
     $(info[0]).fadeIn("fast");
   });
+
   $('.card').mouseleave( function(e) {
     e.preventDefault();
     var target = e.target;
     var info = $(target).children('.card-body');
     $(info[0]).fadeOut("fast");
   });
+
   $(".card").click(function() {
   window.location = $(this).find("a").attr("href");
   return false;
-});
+  });
 
 });
